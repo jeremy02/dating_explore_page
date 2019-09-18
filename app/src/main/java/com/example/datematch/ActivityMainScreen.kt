99019f85cde0cd7a0845ca302c2bcccd1f3731c7
Jeremiah Thuku <jeremiah.thuku@jubileekenya.com>
2019-09-18 16:54:02 +0300

    changed launcher icons

--- app/src/main/java/com/example/datematch/ActivityMainScreen.kt
+++ app/src/main/java/com/example/datematch/ActivityMainScreen.kt
@@ -3,27 +3,25 @@ package com.example.datematch
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 import android.view.View
 import android.view.WindowManager
 import androidx.core.content.ContextCompat
-import kotlinx.android.synthetic.main.activity_fullscreen.*
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-class FullscreenActivity : AppCompatActivity() {
+class ActivityMainScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         // set the theme of statuc bar icons to dark
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)
-//        };
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)
+        };
 
-        setContentView(R.layout.activity_fullscreen)
+        setContentView(R.layout.activity_main_screen)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         // Delayed display of UI elements
